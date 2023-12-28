8093a8a44acc2729042c190ae2136a3f6c5bc3b0
Ofori <roforiantwi@yahoo.com>
2023-12-28 18:30:27 -0500

    first commit

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "server1" {
+ instance_type = var.instance_type 
+ ami = data.aws_ami.ami1.id
+ tags ={
+    Name = "Terraform-server"
+ }
+}
+
+resource "aws_instance" "server2" {
+ instance_type = var.instance_type 
+ ami = data.aws_ami.ami1.id
+ tags ={
+    Name = "Terraform-server2"
+ }
+}
