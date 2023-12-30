39d206ad468403cf5352efa12fb05dd98c2abc9b
Ofori <roforiantwi@yahoo.com>
2023-12-30 08:46:42 -0500

    code

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "server1" {
  instance_type = var.instance_type 
  ami = data.aws_ami.ami1.id
  tags ={
-    Name = "Terraform-server"
+    Name = "Terraform-server2"
  }
 }
 
@@ -10,6 +10,6 @@ resource "aws_instance" "server2" {
  instance_type = var.instance_type 
  ami = data.aws_ami.ami1.id
  tags ={
-    Name = "Terraform-server"
+    Name = "Terraform-server2"
  }
 }
